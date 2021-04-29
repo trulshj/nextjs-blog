e16966f2488480cba435125bc29d5825866e7561
Truls H <fanthoft@gmail.com>
2021-04-29 02:59:06 +0200

    Change word

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ allPostsData }) {
             <section
                 className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
             >
-                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <h2 className={utilStyles.headingLg}>Posts</h2>
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
