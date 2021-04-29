be9b6c8f6a402a6a00a5dbf97e005153745d84bb
Truls H <fanthoft@gmail.com>
2021-04-29 02:42:07 +0200

    url parameters don't work for filepaths, duh

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico?v=2" />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
